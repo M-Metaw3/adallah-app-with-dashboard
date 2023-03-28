1335e072990aa4392315f6ad6a1f9b6b66bf31e0
nadah79 <nadah.hesham.sh@gmail.com>
2023-03-28 15:14:50 +0200

    update

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,38 +1,38 @@
 import React from 'react';
-import { useDispatch ,useSelector } from 'react-redux';
-import { useEffect,useState  } from "react";
-import registrationAction from'../action/postaction'
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from "react";
+import registrationAction from '../action/postaction'
 import { useNavigate } from 'react-router-dom';
 import './style/loggin.css'
 import mmm from '../action/postaction'
+
 const Login = () => {
     // const selector = useSelector((state)=>state)
-// const token = JSON.parse(localStorage.getItem("token"))
-const nav = useNavigate()
+    // const token = JSON.parse(localStorage.getItem("token"))
+    const nav = useNavigate()
 
-const dispatch = useDispatch()
-const [loggin, setloggin] = useState({ email:'',password:''});
+    const dispatch = useDispatch()
+    const [loggin, setloggin] = useState({ email: '', password: '' });
 
-const loginMethod= async() => {
+    const loginMethod = async () => {
 
-await dispatch(   registrationAction.loginAction(loggin))
- nav('/Home')
+        await dispatch(registrationAction.loginAction(loggin))
+        nav('/Home')
 
 
 
-  console.log("done");
-}
-
+        console.log("done");
+    }
 
 
     return (
-        <div className='form'>
-                  <label htmlFor="email">email</label>
-       <input value={loggin.email}  onChange={(e)=>setloggin({...loggin,email:e.target.value})} type="email" name="email" id="email" />
-       <label htmlFor="pass">password</label>
-       <input value={loggin.password}  min={"9"} onChange={(e)=>setloggin({...loggin,password:e.target.value})} type="password" name="pass" id="pass" />
-      <button onClick={loginMethod}>Login</button>
-        </div>
+        <form action="" className='form'>
+            <h3 className=''>عدالة</h3>
+            <input className="form-control" value={loggin.email} onChange={(e) => setloggin({ ...loggin, email: e.target.value })} type="email" name="email" id="email" placeholder='email' />
+            <input className="form-control" value={loggin.password} min={"9"} onChange={(e) => setloggin({ ...loggin, password: e.target.value })} type="password" name="pass" id="pass" placeholder='password' />
+            <button className="btn btn-primary" onClick={loginMethod}>Login</button>
+
+        </form>
     );
 }
 
