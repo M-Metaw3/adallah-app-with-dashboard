71d7e806dfb28c3dd679e4990cdeab09dafc085b
M-Metaw3 <mizoomizoo161@gmail.com>
2023-04-25 07:14:03 +0200

    locations

--- backend/index.js
+++ backend/index.js
@@ -5,5 +5,5 @@ const app=require('./app/src')
 const port=process.env.PORT||7000
 
 app.listen(port,()=>{
-    console.log('now we listen to port:'+port)
+    console.log('now  listen to port:'+port)
 })
